2e8ec2f49e34e7dd26f1e32dc4e01368a87ccb98
Darwin Boersma <darwin@sprout.org>
2024-09-03 16:37:14 -0600

    Simplify poller structure, update error messages
    Signed-off-by: Darwin Boersma <darwin@sprout.org>

--- src/aws.rs
+++ src/aws.rs
@@ -1,61 +1,41 @@
-use std::marker::PhantomData;
-
 use aws_sdk_kinesis::{types::Record, Client};
 
-pub struct New;
-pub struct Ready;
-
-pub struct ShardPoller<'a, PollerState = New> {
+pub struct ShardPoller<'a> {
     client: Client,
     stream_arn: &'a str,
     shard_id: &'a str,
     shard_iterator: Option<String>,
     shard_record_limit: u16,
-    _state: PhantomData<PollerState>,
 }
 
-impl<'a> ShardPoller<'a, New> {
-    pub fn new(
+impl<'a> ShardPoller<'a> {
+    /// Try to create a new poller for the given stream and shard
+    pub async fn try_new(
         client: Client,
         stream_arn: &'a str,
         shard_id: &'a str,
         shard_record_limit: u16,
-    ) -> Self {
-        Self {
-            client,
-            stream_arn,
-            shard_id,
-            shard_iterator: None,
-            shard_record_limit,
-            _state: PhantomData,
-        }
-    }
-
-    pub async fn make_ready(mut self) -> anyhow::Result<ShardPoller<'a, Ready>> {
-        let shard_iterator = self
-            .client
+    ) -> anyhow::Result<Self> {
+        let shard_iterator = client
             .get_shard_iterator()
-            .stream_arn(self.stream_arn)
-            .shard_id(self.shard_id)
+            .stream_arn(stream_arn)
+            .shard_id(shard_id)
             .shard_iterator_type(aws_sdk_kinesis::types::ShardIteratorType::Latest)
             .send()
             .await
             .map(|res| res.shard_iterator)
             .map_err(anyhow::Error::from)?;
-        self.shard_iterator = shard_iterator;
 
-        Ok(ShardPoller {
-            client: self.client,
-            stream_arn: self.stream_arn,
-            shard_id: self.shard_id,
-            shard_iterator: self.shard_iterator,
-            shard_record_limit: self.shard_record_limit,
-            _state: PhantomData,
+        Ok(Self {
+            client,
+            stream_arn,
+            shard_id,
+            shard_iterator,
+            shard_record_limit,
         })
     }
-}
 
-impl ShardPoller<'_, Ready> {
+    /// Get records from the shard using this poller. This will return an empty vector if there is no shard iterator
     pub async fn get_records(&mut self) -> Vec<Record> {
         if let Some(shard_iterator) = &self.shard_iterator {
             match self
@@ -77,7 +57,10 @@ impl ShardPoller<'_, Ready> {
                 }
             }
         } else {
-            tracing::trace!("No records in shard {}", self.shard_id);
+            tracing::trace!(
+                "Null shard iterator for poller {}. TODO: We should stop polling this iterator",
+                self.shard_id
+            );
             vec![]
         }
     }
