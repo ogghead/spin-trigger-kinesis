215da8648e837516e977e1f7ca9c92d28cec80d7
MacKenzie Olson <mackenzie.olson@fermyon.com>
2024-10-03 14:39:46 -0700

    Add template experience for rust users (#11)
    * added template directory with content
    * replaced files in content directory, started spin-template.toml
    * whoops missed the changes
    * modified types in spin-template.toml
    
    ---------
    
    Signed-off-by: macolso <mackenzie.olson@fermyon.com>

---
+++ templates/kinesis-rust/content/src/lib.rs
@@ -0,0 +1,12 @@
+use spin_kinesis_sdk::{kinesis_component, Error, KinesisRecord};
+
+#[kinesis_component]
+async fn handle_batch_records(records: Vec<KinesisRecord>) -> Result<(), Error> {
+    for record in records {
+        println!("I GOT A RECORD!  ID: {:?}", record.sequence_number);
+        let data = String::from_utf8(record.data.inner).unwrap();
+        println!("  ... DATA: {:?}", data);
+    }
+
+    Ok(())
+}
